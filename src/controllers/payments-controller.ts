ae28a0b4a7f20da7a91f993677a8b2ec971c6c12
Pedro <malpvpandtextures@gmail.com>
2023-01-27 01:16:14 -0300

    bug fix rota /payments

--- src/controllers/payments-controller.ts
+++ src/controllers/payments-controller.ts
@@ -3,10 +3,13 @@ import { Request, Response } from "express";
 import httpStatus from "http-status";
 
 export async function getPaymentByTicketId(req: Request, res: Response) {
-  const { ticketId } = req.body;
+  const { ticketId } = req.query;
+  if(!ticketId) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
   const { userId } = req.body;
   try{
-    const paymentById = await getPaymentById(ticketId, userId);
+    const paymentById = await getPaymentById(+ticketId, userId);
     return res.send(paymentById);
   }
   catch(error) {
